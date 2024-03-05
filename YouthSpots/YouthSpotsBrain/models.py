c4f539c3f446905a016362928992742c8dc1ea15
Matthijs <matthijs96graaf@gmail.com>
2024-03-05 14:57:41 +0100

    Change the setting for databases and make the maps work

--- YouthSpots/YouthSpotsBrain/models.py
+++ YouthSpots/YouthSpotsBrain/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class EVCharcinglocation(models.Model):
+    station_name = models.CharField(max_length=250)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.station_name
--- YouthSpots/YouthSpotsBrain/views.py
+++ YouthSpots/YouthSpotsBrain/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from YouthSpotsBrain.models import EVCharcinglocation
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def maps(request):
+    stations = list(EVCharcinglocation.objects.values('latitude', 'longitude')[:100])
+    print(stations[:2])
+    context = {'stations':stations}
+    return render(request, "maps.html", context)
