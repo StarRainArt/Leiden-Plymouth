bff3405543e365907b09ff36471a3623bc2274f0
Matthijs <matthijs96graaf@gmail.com>
2024-03-07 12:57:27 +0100

    block content instead of map

--- YouthSpots/YouthSpotsBrain/models.py
+++ YouthSpots/YouthSpotsBrain/models.py
@@ -1,9 +1,31 @@
 from django.db import models
 
-class EVCharcinglocation(models.Model):
-    station_name = models.CharField(max_length=250)
-    latitude = models.FloatField()
+class Pins(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tags = models.ManyToManyField('Tag')
     longitude = models.FloatField()
+    latitude = models.FloatField()
+    owner_id = models.IntegerField()
+    invited = models.CharField(max_length=255)
+    created_timestamp = models.DateTimeField()
 
     def __str__(self):
-        return self.station_name
+        return self
+    
+class Meetups(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    start_timestamp = models.DateTimeField()
+    end_timestamp = models.DateTimeField()
+    tags = models.ManyToManyField('Tag')
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    owner_id = models.IntegerField()
+    invited = models.CharField(max_length=255)
+    created_timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return self
--- YouthSpots/YouthSpotsBrain/views.py
+++ YouthSpots/YouthSpotsBrain/views.py
@@ -1,13 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-from YouthSpotsBrain.models import EVCharcinglocation
+from YouthSpotsBrain.models import Meetups
+from YouthSpotsBrain.models import Pins
 from geopy.distance import geodesic
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def maps(request):
-    stations = list(EVCharcinglocation.objects.values('latitude', 'longitude')[:100])
+    stations = list(Pins.objects.values('latitude', 'longitude')[:100])
     # print(stations[:2])
     context = {'stations':stations}
     return render(request, "maps.html", context)
@@ -17,7 +18,7 @@ def nearest_station(request):
     longitude = request.GET.get('longitude')
     user_location = latitude, longitude
     station_distances = {}
-    for station in EVCharcinglocation.objects.all()[:100]:
+    for station in Pins.objects.all()[:100]:
         station_location = station.latitude, station.longitude
         
         distance = geodesic(user_location, station_location).km
