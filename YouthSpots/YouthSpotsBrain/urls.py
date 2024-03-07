1f6ff4f3f55bda4a10dbfe4823777f8734b6f7f3
Matthijs <matthijs96graaf@gmail.com>
2024-03-07 09:41:58 +0100

    Made a polyline between the user and marker when clicked

--- YouthSpots/YouthSpotsBrain/urls.py
+++ YouthSpots/YouthSpotsBrain/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("map/", views.maps)
+    path("map/", views.maps),
+    path('get-nearest-station/', views.nearest_station)
 ]
--- YouthSpots/YouthSpotsBrain/views.py
+++ YouthSpots/YouthSpotsBrain/views.py
@@ -1,12 +1,30 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from YouthSpotsBrain.models import EVCharcinglocation
-
+from geopy.distance import geodesic
 # Create your views here.
 def home(request):
     return render(request, "home.html")
 
 def maps(request):
     stations = list(EVCharcinglocation.objects.values('latitude', 'longitude')[:100])
-    print(stations[:2])
+    # print(stations[:2])
     context = {'stations':stations}
     return render(request, "maps.html", context)
+
+def nearest_station(request):
+    latitude = request.GET.get('latitude')
+    longitude = request.GET.get('longitude')
+    user_location = latitude, longitude
+    station_distances = {}
+    for station in EVCharcinglocation.objects.all()[:100]:
+        station_location = station.latitude, station.longitude
+        
+        distance = geodesic(user_location, station_location).km
+        station_location[distance] = station_location
+    min_distances = min(station_distances)
+    station_coords = station_distances(min_distances)
+    return JsonResponse({
+        'coordinates': station_coords,
+        'distance': min_distances
+    })
