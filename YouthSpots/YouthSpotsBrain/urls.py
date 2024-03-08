a9262fbee2bb613114f49ad68aaa4fc14410def9
Matthijs <matthijs96graaf@gmail.com>
2024-03-08 16:04:05 +0100

    Pushed research map

--- YouthSpots/YouthSpotsBrain/urls.py
+++ YouthSpots/YouthSpotsBrain/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("map/", views.maps),
-    path('get-nearest-pin/', views.nearest_pin),
     path('api/save-marker', views.save_marker, name='save_marker'),
     path("login/", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("logout/", views.logout, name="logout"),
+    path("getPins/", views.getPins),
 ]
--- YouthSpots/YouthSpotsBrain/views.py
+++ YouthSpots/YouthSpotsBrain/views.py
@@ -15,33 +15,10 @@ def home(request):
     return render(request, "home.html")
 
 def maps(request):
-    pins = list(Pins.objects.values('id', 'title', 'description', 'latitude', 'longitude', 'tags', 'created_timestamp')[:100])
-    # print(pins[:2])
-    context = {'pins':pins}
-    return render(request, "maps.html", context)
+    return render(request, "maps.html")
 
-def nearest_pin(request):
-    latitude = request.GET.get('latitude')
-    longitude = request.GET.get('longitude')
-    user_location = (latitude, longitude)
-    pin_distances = {}
-
-    for pin in Pins.objects.all()[:100]:
-        pin_location = (pin.latitude, pin.longitude)
-        distance = geodesic(user_location, pin_location).km
-        pin_distances[distance] = { 'location': pin_location, 'distance': distance }
-
-    if pin_distances:
-        min_distances = min(pin_distances)
-        pin_coords = pin_distances[min_distances]
-    else:
-        min_distances = None
-        pin_coords = None
-
-    return JsonResponse({
-        'coordinates': pin_coords,
-        'distance': min_distances
-    })
+def getPins(request):
+    return JsonResponse(list(Pins.objects.values('id', 'title', 'description', 'latitude', 'longitude', 'tags', 'created_timestamp')[:100]), safe=False)
 
 
 def save_marker(request):
