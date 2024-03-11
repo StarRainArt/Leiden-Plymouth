0e3c44f6b4b72163a89483086e2761d6dcad9097
joepk8 <joepjfklinkenberg@gmail.com>
2024-03-11 11:24:05 +0000

    Bloodyhell

--- YouthSpots/YouthSpotsBrain/urls.py
+++ YouthSpots/YouthSpotsBrain/urls.py
@@ -4,15 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("map/", views.maps),
-<<<<<<< HEAD
-=======
     path("getPins/", views.getPins),
     path("savePin/", views.savePin),
     path('get-nearest-pin/', views.nearest_pin),
->>>>>>> 96d644f38692747e7cf03a4329242c5130ad47c9
     path('api/save-marker', views.save_marker, name='save_marker'),
     path("login/", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("logout/", views.logout, name="logout"),
-    path("getPins/", views.getPins),
 ]
