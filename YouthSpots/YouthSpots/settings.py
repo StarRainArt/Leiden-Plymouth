724792ad5d1eaa94609f05df14acbe3f41d5a7a4
StarRainArt <118188985+StarRainArt@users.noreply.github.com>
2024-03-11 13:45:49 +0000

    minor change icons

--- YouthSpots/YouthSpots/settings.py
+++ YouthSpots/YouthSpots/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'socialsphere',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
