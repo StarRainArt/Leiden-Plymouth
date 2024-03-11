b06c7cb31d4661ef8d7a351983793fc7fb08c21f
StarRainArt <118188985+StarRainArt@users.noreply.github.com>
2024-03-11 13:58:13 +0000

    changed main page

--- YouthSpots/YouthSpotsBrain/urls.py
+++ YouthSpots/YouthSpotsBrain/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", views.maps, name="map"),
+    path("testpage/", views.home, name="home"),
     path("profile/", views.profile, name="profile"),
-    path("map/", views.maps),
     path('get-nearest-station/', views.nearest_station)
 ]
