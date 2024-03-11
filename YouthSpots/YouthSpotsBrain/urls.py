b052f9bde59e6b8be55c03adf86ccfc9cfe6e439
StarRainArt <118188985+StarRainArt@users.noreply.github.com>
2024-03-11 14:21:34 +0000

    nav to available pages

--- YouthSpots/YouthSpotsBrain/urls.py
+++ YouthSpots/YouthSpotsBrain/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.maps, name="map"),
-    path("testpage/", views.home, name="home"),
-    path("profile/", views.profile, name="profile"),
-    path('get-nearest-station/', views.nearest_station)
+    # path("meetups/", views.meetups, name="meetups"),
+    # path("pins/", views.pins, name="pins"),
+    # path("profile/", views.profile, name="profile"),
+    path("profile-edit/", views.profile, name="profile-edit"),
+    # path("settings/", views.settings, name="settings"),
+    path('get-nearest-station/', views.nearest_station),
+    path("testpage/", views.home, name="test"),
 ]
