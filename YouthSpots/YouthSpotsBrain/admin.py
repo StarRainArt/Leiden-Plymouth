39068c7eb78b06d9067390d32b37e15f625c64c6
joepk8 <joepjfklinkenberg@gmail.com>
2024-03-14 10:17:18 +0000

    admin page fixed

--- YouthSpots/YouthSpotsBrain/admin.py
+++ YouthSpots/YouthSpotsBrain/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Pins, Tags, Profile, Meetups, UserAuth
 
+admin.site.register(Pins)
+admin.site.register(Tags)
+admin.site.register(Profile)
+admin.site.register(Meetups)
+admin.site.register(UserAuth)
 
 
 # Register your models here.
