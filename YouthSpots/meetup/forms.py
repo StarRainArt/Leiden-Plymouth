6923e03a57e24116a8df3f1d5c6f1532b4b39518
SireSem <6029228@mborijnland.nl>
2024-03-14 10:49:25 +0000

    .

--- YouthSpots/meetup/forms.py
+++ YouthSpots/meetup/forms.py
@@ -3,7 +3,7 @@ from .models import Meetups
 from YouthSpotsBrain.models import Pins
 class MeetupsForm(forms.ModelForm):
     location = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
-    name_meetup = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    #name_meetup = forms.CharField(widget=forms.TextInput(attrs={'readonly': 'readonly'}))
     class Meta:
         model = Meetups
         fields = ['location', 'name_meetup', 'description', 'time_start', 'time_end', 'invited', 'tags', 'pin', 'visibility']
--- YouthSpots/meetup/urls.py
+++ YouthSpots/meetup/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     #path("meetup_select/<int:meetup_id>/", views.edit_meetup, name="select_meetup"),
     #path('edit_meetup_details/<str:meetup_id>/', views.edit_meetup_details, name='edit_meetup_details'),
-    #path('my_meetups/', views.my_meetups, name='my_meetups'),
-    #path('meetup_public/', views.public_meetups, name='public_meetups'),
+    path('my_meetups/', views.my_meetups, name='my_meetups'),
+    path('meetup_public/', views.public_meetups, name='public_meetups'),
     path("meetup/", views.meetup, name="meetup"),
     path('api/pin/<int:pin_id>/', views.get_pin, name='get_pin'),
     # path("create_meetup/", views.meetup_data_create, name="create_meetup"),
