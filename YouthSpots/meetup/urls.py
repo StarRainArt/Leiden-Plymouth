9cf717a012ca81acbed5dc828ff5697c45f2966a
Matthijs <matthijs96graaf@gmail.com>
2024-03-15 11:12:26 +0100

    Made the edit profile work

--- YouthSpots/meetup/urls.py
+++ YouthSpots/meetup/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-# from YouthSpotsBrain import views
+
 urlpatterns = [
     path("meetup_select/", views.select_meetup, name="select_meetup"),
     path('edit_meetup_details/', views.edit_meetup_details, name='edit_meetup_details'),
@@ -8,10 +8,9 @@ urlpatterns = [
     path('my_meetups/', views.my_meetups, name='my_meetups'),
     path('meetup_public/', views.public_meetups, name='public_meetups'),
     path('meetup_private/', views.private_meetups, name='private_meetups'),
+    path('meetup_protected/', views.protected_meetups, name='protected_meetups'),
     path("meetup/", views.meetup, name="meetup"),
     path("meetup_delete/", views.delete_meetup, name="delete_meetup"),
-    # path('api/pin/<int:pin_id>/', views.get_pin, name='get_pin'),
-    # path("create_meetup/", views.meetup_data_create, name="create_meetup"),
 ]
 
     
